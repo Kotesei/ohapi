38a08031c78fd666bb2e02aa21d369363ed1eb06
Kotesei <192398548+Kotesei@users.noreply.github.com>
2026-04-26 13:00:27 -0400

    Changed loop method and created a start/end point for seperating units and storing them in chunks instead of one giant array.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ async function fetchUnits(groups) {
       let res = await fetch("http://localhost:3000/")
       let data = await res.json();
       const API_KEY = data.key
-      // Loop through tabs to get all unit data
+      // Loop through tabs to get all unit data (Do NOT use yet)
       // await Promise.all(
       // groups.map(async (group) => {
       //    const endpoint = `${URL_LINK}${SHEET_ID}/values/${group}?valueRenderOption=FORMULA&key=${API_KEY}`;
@@ -23,16 +23,53 @@ async function fetchUnits(groups) {
       //    console.log(data)
       //    })
       // )
+      
       // Create a structure for the data per tab before using the promie.all method above (Reduces amount of API calls while structuring)
          const endpoint = `${URL_LINK}${SHEET_ID}/values/${groups[1]}?valueRenderOption=FORMULA&key=${API_KEY}`;
          res = await fetch(endpoint);
          data = await res.json();
-         console.log(data)
+         let unitDataStartingPoints = []
+         let unitDataEndingPoints = []
+         const unitData = []
 
+         // Create starting and end points for the units
+         data.values.map((row, pos) => {
+            if (row.includes("Splash Art")){
+               unitDataStartingPoints.push(pos)
+            }
+            if (row.length < 1 && data.values[pos + 1][0] && !data.values[pos + 1][0].includes("=")) {
+               unitDataEndingPoints.push(pos)
+            }
+         })
+         // Push the last index value for the last unit in the sheet
+         unitDataEndingPoints.push(data.values.length)
+
+         // Push the unit data by using the slice method to seperate them into their own array since there is no seperator pattern found.
+         unitDataStartingPoints.map((_, pos) => {
+            unitData.push(data.values.slice(unitDataStartingPoints[pos], unitDataEndingPoints[pos]))
+         })
+
+         unitData.map(unit => {
+            console.log(unit)
+            const name = unit[0][0]
+            const awakeningAccessoryName = unit[0][21]
+            const awakeningAccessoryDecription = unit[1][23]
+
+            // To-do: through skills and get the SP cost and description
+            // Also create a relation-link object that understands parts like =HP =SP etc.. in the sheet
+            // Clean out any empty arrays from the unit array. 
+
+            units.push({
+               name: name,
+               awakeningAccessoryName: awakeningAccessoryName,
+               awakeningAccessoryDecription: awakeningAccessoryDecription
+            })
+         })
 
    } catch (error) {
       console.error("Error:", error)
    }
 }
 
-fetchUnits(unitGroups)
+await fetchUnits(unitGroups)
+console.log(units)
