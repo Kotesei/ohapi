c4a9592fc03bdf1273b2f33c862a4e520abeb3e2
Kotesei <192398548+Kotesei@users.noreply.github.com>
2026-06-19 21:18:26 -0400

    Added blobs and elements for easier use

--- getters.js
+++ getters.js
@@ -17,22 +17,34 @@ export async function getCharacter(
     const res = await fetch(`${HOSTURL}/api/character/${name}`);
     const unit = await res.json();
     let imgs = [];
+    let elements = [];
     let spriteData;
+    let spriteEl;
     if (sprite) {
       const spriteRes = await fetch(`${HOSTURL}/api/character/${name}/sprite`);
       spriteData = await spriteRes.blob();
       imgs.push({ sprite: spriteData });
+      spriteEl = document.createElement("img");
+      spriteEl.src = URL.createObjectURL(spriteData);
+      spriteEl.classList.add("character_spriteImg");
+      elements.push(spriteEl);
     }
     let bannerData;
+    let bannerEl;
     if (banner) {
       const bannerRes = await fetch(`${HOSTURL}/api/character/${name}/banner`);
       bannerData = await bannerRes.blob();
       imgs.push({ banner: bannerData });
+      bannerEl = document.createElement("img");
+      bannerEl.src = URL.createObjectURL(bannerData);
+      bannerEl.classList.add("character_bannerImg");
+      elements.push(bannerEl);
     }
 
     const unitData = {
       unit,
       ...(imgs.length > 0 ? { imgs } : null),
+      ...(elements.length > 0 ? { elements } : null),
     };
 
     return unitData;
